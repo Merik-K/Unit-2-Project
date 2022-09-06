a065ef11ab028721d23fd174b5c3ca9fc1bf624a
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2022-09-06 16:48:49 -0700

    Added Binary Converter
    Made the binary converter, which makes the number into a binary string. I also made it so the decimal number converts into a number instead of a string.

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,9 @@
 let decimalNumber = prompt("Enter the decimal number you want to be converted into hexidecimal and binary here:")
-
+decimalNumber = parseInt(decimalNumber)
 //hexadecimal:
 
 
 //binary:
+let binaryNumber = decimalNumber.toString(2);
+
+console.log(binaryNumber)
