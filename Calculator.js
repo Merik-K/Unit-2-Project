20e2028695e604a947773451ca4e46275a19c9b8
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2022-09-12 14:47:22 -0700

    Added HTML, Javascript, and CSS
    Added the code to make it so when you press the calculate button the answer will be printed to the html. Centered everything on the page, added margins so the paragraph looks better, and I moved the title down a bit. I also moved the calculate button and text box to below the paragraph that explains what the website does.

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,14 @@
 function numberInputted() {
-var decimalNumber = document.getElementById(decimalNumberHTML)
+console.log(document.getElementById("decimalNumberHTML").value)
+    var decimalNumber = document.getElementById("decimalNumberHTML").value
+
 decimalNumber = parseInt(decimalNumber)
 //hexadecimal:
-let hexDec = decimalNumber.toString(16);
-console.log(hexDec)
+let hexNumber = decimalNumber.toString(16);
+document.getElementById("hexNumberHTML").innerHTML = hexNumber
 
 //binary:
 var binaryNumber = decimalNumber.toString(2);
+document.getElementById("binaryNumberHTML").innerHTML = binaryNumber
 
-console.log(binaryNumber)
 }
