e5326652323a1edcbaf1655bc6559971782f750d
MerikKuhn <mkuhn181@west-mec.org>
2022-09-12 18:01:16 -0700

    making comments and a few fixes
    Made comments to show what each code does and make a few fixes to the code.

--- Calculator.js
+++ Calculator.js
@@ -3,10 +3,11 @@ console.log(document.getElementById("decimalNumberHTML").value)
     var decimalNumber = document.getElementById("decimalNumberHTML").value
 
 decimalNumber = parseInt(decimalNumber)
+//code for tranfering decimal to hexadecimal.
 //hexadecimal:
 let hexNumber = decimalNumber.toString(16);
 document.getElementById("hexNumberHTML").innerHTML = hexNumber
-
+//code for tranfering decimal to binary.
 //binary:
 var binaryNumber = decimalNumber.toString(2);
 document.getElementById("binaryNumberHTML").innerHTML = binaryNumber
